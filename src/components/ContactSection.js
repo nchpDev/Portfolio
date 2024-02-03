173e056c60bdc756b8cc4d102ad93e3480a3a336
nchp <nacho.porretta00@gmail.com>
2024-02-03 12:28:26 -0300

    sv updates

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -26,7 +26,7 @@ const ContactSection = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3001/submit-form', {
+      const response = await fetch('https://portfolio-mu-murex-79.vercel.app/submit-form', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
