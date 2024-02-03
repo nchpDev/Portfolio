53a87fc4d6cf7e9c5d012680e5b9fc79ecfd3f85
nchp <nacho.porretta00@gmail.com>
2024-02-03 12:23:54 -0300

    updates for sv

---
+++ api/submit-form.js
@@ -0,0 +1,51 @@
+// api/submit-form.js
+const nodemailer = require('nodemailer');
+
+module.exports = async (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+
+    try {
+        const { name, lastname, email, phone, company, description } = req.body;
+        console.log('Received data:', req.body);
+
+        // Use nodemailer to send an email
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+            authMethod: 'PLAIN',
+            logger: true,
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_FROM,
+            to: process.env.EMAIL_TO,
+            subject: 'Contact-Me Form Submission',
+            text: `
+            Name: ${name}
+            Lastname: ${lastname}
+            Email: ${email}
+            Phone: ${phone}
+            Company: ${company}
+            Description: ${description}
+            `,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+};
